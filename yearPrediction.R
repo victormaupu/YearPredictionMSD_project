b5fcd5ff73e3ba33bb663c853268198cad720820
Victor Maupu <victor.maupu@gmail.com>
2018-12-08 17:38:13 +0100

    function createSVM

--- yearPrediction.R
+++ yearPrediction.R
@@ -4,3 +4,11 @@ importCSV = function(name)
   dataCSV <- as.matrix(importCSV)
   return(dataCSV)
 }
+
+createSVM = function(dataCSV)
+{
+  x <- subset(dataCSV,select=-V1)
+  y <- subset(dataCSV,select=V1)
+  model <- svm(x,y,cost=10,gamma=0.5)
+  return(model)
+}
